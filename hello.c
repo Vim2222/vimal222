c19baf20b1c8acff6a5bb3b9886b05ed36b71431
Vim2222 <113259519+Vim2222@users.noreply.github.com>
2023-06-05 07:12:08 +0530

    please review (#2)

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a=20,b=30;
+	int c=a+b;
+	printf("Enter the addition of two numbers : %d + %d = %d \n ",a,b,c);
+	print("\n");
+	return 0;
+}
+
